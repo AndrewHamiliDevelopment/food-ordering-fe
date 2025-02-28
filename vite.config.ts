6db0dfd9310eb87e581bec4a2c89763f8b900e79
andrewhamili <andrewhamilidevelopment@gmail.com>
2025-02-28 21:27:05 +0800

    Vite config to proxy staging API

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+  console.log("🚀 ~ mode:", mode)
+  console.log("🚀 ~ STAGING_API_URL:", process.env.STAGING_API_URL)
+  
+
+  return defineConfig({
+    server: {
+        proxy: {
+          '/api': {
+            secure: false,
+            changeOrigin: true,
+            target: process.env.STAGING_API_URL
+          }
+        }
+    },
+      plugins: [react()],
+  });
+}
+  
